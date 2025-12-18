24cefea84e51c95fa2037f41a8a2c12897b8ca66
doubletl22 <kiyoshineko41@gmail.com>
2025-12-18 21:43:24 +0700

    Base code, thêm thư viện, add core

---
+++ backend/src/BuildingBlocks/S2O.Shared.Kernel/Primitives/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace S2O.Shared.Kernel.Primitives;
+
+// Domain Event là 1 Notification trong MediatR
+public interface IDomainEvent : INotification
+{
+    Guid EventId { get; }
+    DateTime OccurredOn { get; }
+}
