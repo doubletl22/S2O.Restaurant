05e20d64368854c5a3a582775445e7e15ee56931
huynhdaothanhtung-052205001661 <tungtru2k5@gmail.com>
2025-12-24 02:39:27 +0700

    Delete AIService/backend/AIService directory

--- AIService/backend/AIService/Contracts/IngestDocRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace S2O.AIService.Contracts;
-
-public sealed class IngestDocRequest
-{
-    public required string TenantId { get; set; }
-    public required string RestaurantId { get; set; }
-    public required string Title { get; set; }
-    public required string Content { get; set; }
-}
