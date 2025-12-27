743b3f417d9f8d2645aa18b12d1be16247a36434
Nguyen Tan Vinh <nguyentanvinh0403@gmail.com>
2025-12-27 12:47:17 +0700

    Tenan tservice

---
+++ backend/src/Services/Tenant/S20.Services.Tenant.Application/Interfaces/ITenantServices.cs
@@ -0,0 +1,14 @@
+﻿using S20.Services.Tenants.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S20.Services.Tenants.Application.Interfaces
+{
+    public interface ITenantServices
+    {
+        Task<TenantResponse> CreateTenantAsync(CreateTenantRequest request, Guid systemAdminUserId);
+    }
+}
