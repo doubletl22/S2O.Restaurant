7b6275ea6761988e652dd18f2027fc45f9f55a54
doubletl22 <kiyoshineko41@gmail.com>
2026-01-03 08:38:36 +0700

    kiến trúc sạch sửa đổi

--- backend/src/BuildingBlocks/S2O.Shared.Kernel/CQRS/IQuery.cs
+++ backend/src/BuildingBlocks/S2O.Shared.Kernel/CQRS/IQuery.cs
@@ -1,10 +1,8 @@
 ﻿using MediatR;
 using S2O.Shared.Kernel.Wrapper;
 
-namespace S2O.Shared.Kernel.CQRS;
-
-// Query BẮT BUỘC phải trả về value (VD: GetUserByIdQuery -> trả về UserDto)
-// Không bao giờ có chuyện Query trả về void (IRequest<Result>)
-public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+namespace S2O.Shared.Kernel.CQRS
 {
+    // Query luôn luôn phải trả về kết quả TResponse được bọc trong Result
+    public interface IQuery<TResponse> : IRequest<Result<TResponse>> { }
 }
