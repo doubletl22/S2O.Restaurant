d8fcf48fcf8e2720039ee07ea45de11cfeb8372d
doubletl22 <kiyoshineko41@gmail.com>
2026-01-21 22:51:04 +0700

    chức năng admin

--- backend/src/Services/Tenant/S2O.Tenant.Infra/Migrations/20260121154753_AddLockColumnToTenant.Designer.cs
+++ backend/src/Services/Tenant/S2O.Tenant.Infra/Migrations/20260121154753_AddLockColumnToTenant.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using S2O.Tenant.Infra.Persistence;
@@ -11,9 +12,11 @@ using S2O.Tenant.Infra.Persistence;
 namespace S2O.Tenant.Infra.Migrations
 {
     [DbContext(typeof(TenantDbContext))]
-    partial class TenantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260121154753_AddLockColumnToTenant")]
+    partial class AddLockColumnToTenant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
